7d5ed02284e541b4eec0f6020c78ad4f664c5822
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-12 19:17:55 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,7 @@
+name="Sharadha"
+age=23
+name="Khushnood"
+isAdult=True
+print(isAdult)
+print(name)
+print(age)
---
+++ WhileLoop.py
@@ -0,0 +1,22 @@
+# i=1
+# while i<=5:
+#     print(i)
+#     i+=1
+
+
+
+# Triangle pattern using While Loop:
+
+# i=1
+# while i<=5:
+#     print(i * " * ")
+#     i+=1
+
+
+
+# Reverse the triangle pattern using While Loop:
+
+i=5
+while i>=1:
+    print(i * " * ")
+    i-=1
