0137da649a6af75efcb2155501de23d11dcdc191
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-12 19:15:35 +0530

    Add files via upload

---
+++ Break&Continue.py
@@ -0,0 +1,23 @@
+# Using Break:
+
+
+# rollno=[1,2,3,4,5,6,7,8,9,10]
+# for i in rollno:
+#     if i==5:
+#         break
+#     print(i)
+
+
+# Using Continue:
+
+rollno=[1,2,3,4,5,6,7,8,9,10]
+for i in rollno:
+    if i==5:
+        continue
+    print(i)
+
+
+
+
+
+
---
+++ Comparison Operators.py
@@ -0,0 +1,7 @@
+print(3>45)
+
+print(9<1)
+
+print(2==2)
+
+print(23>14)
