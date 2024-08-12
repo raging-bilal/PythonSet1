ac84c5383ccb2c13f16eeced3ca36bcd8da41073
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-08-12 19:17:38 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,11 @@
+# NOTE: Tuples are immutable!
+
+
+age=(12,34,34,56,78,90)
+f1=age.count(34)
+
+f2=age.index(78)
+
+
+print(f1)
+print(f2)
---
+++ TypeConversion.py
@@ -0,0 +1,6 @@
+oldAge=input("Enter your old age: ")
+
+print("My old age was: "+ oldAge)
+
+newAge=int(oldAge)+2
+print("My new age is: " + str(newAge))
